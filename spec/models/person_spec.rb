b28a6714ee3991e064c74a4a8dd5c526c8c436eb
Prabhakar Undurthi <undurthi_prabhakar@aol.com>
2014-12-30 12:39:28 +0530

    Added controller tests for CRUD.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without the first_name" do
+    person = Person.new(first_name: nil)
+    expect(person).not_to be_valid
+  end
+
+  it "is invalid without the last_name" do
+    person = Person.new(first_name: 'prabhakar', last_name: nil)
+    expect(person).not_to be_valid
+  end
+
+  it "the first_name should be more then 3 characters" do
+    person = Person.new(first_name: 'bb', last_name: 'undurthi')
+    expect(person).not_to be_valid
+  end
+
+  it "the last_name must be more than 3 characters" do
+    person = Person.new(first_name: 'prabhakar', last_name: 'bn')
+    expect(person).not_to be_valid
+  end
 end
