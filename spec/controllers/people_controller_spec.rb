176127bb0d5e772ee8ee17f9d6f6b2415968e756
Prabhakar Undurthi <undurthi_prabhakar@aol.com>
2014-12-30 12:40:37 +0530

    Added controller tests for CRUD.

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -91,82 +91,79 @@ describe PeopleController do
     end
   end
 
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved person as @person" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        Person.any_instance.stub(:save).and_return(false)
+        post :create, {:person => { "first_name" => "invalid value" }}, valid_session
+        assigns(:person).should be_a_new(Person)
+      end
 
-  #
-  #   describe "with invalid params" do
-  #     it "assigns a newly created but unsaved person as @person" do
-  #       # Trigger the behavior that occurs when invalid params are submitted
-  #       Person.any_instance.stub(:save).and_return(false)
-  #       post :create, {:person => { "first_name" => "invalid value" }}, valid_session
-  #       assigns(:person).should be_a_new(Person)
-  #     end
-  #
-  #     it "re-renders the 'new' template" do
-  #       # Trigger the behavior that occurs when invalid params are submitted
-  #       Person.any_instance.stub(:save).and_return(false)
-  #       post :create, {:person => { "first_name" => "invalid value" }}, valid_session
-  #       response.should render_template("new")
-  #     end
-  #   end
-  # end
-  #
-  # describe "PUT update" do
-  #   describe "with valid params" do
-  #     it "updates the requested person" do
-  #       person = Person.create! valid_attributes
-  #       # Assuming there are no other people in the database, this
-  #       # specifies that the Person created on the previous line
-  #       # receives the :update_attributes message with whatever params are
-  #       # submitted in the request.
-  #       Person.any_instance.should_receive(:update).with({ "first_name" => "MyString" })
-  #       put :update, {:id => person.to_param, :person => { "first_name" => "MyString" }}, valid_session
-  #     end
-  #
-  #     it "assigns the requested person as @person" do
-  #       person = Person.create! valid_attributes
-  #       put :update, {:id => person.to_param, :person => valid_attributes}, valid_session
-  #       assigns(:person).should eq(person)
-  #     end
-  #
-  #     it "redirects to the person" do
-  #       person = Person.create! valid_attributes
-  #       put :update, {:id => person.to_param, :person => valid_attributes}, valid_session
-  #       response.should redirect_to(person)
-  #     end
-  #   end
-  #
-  #   describe "with invalid params" do
-  #     it "assigns the person as @person" do
-  #       person = Person.create! valid_attributes
-  #       # Trigger the behavior that occurs when invalid params are submitted
-  #       Person.any_instance.stub(:save).and_return(false)
-  #       put :update, {:id => person.to_param, :person => { "first_name" => "invalid value" }}, valid_session
-  #       assigns(:person).should eq(person)
-  #     end
-  #
-  #     it "re-renders the 'edit' template" do
-  #       person = Person.create! valid_attributes
-  #       # Trigger the behavior that occurs when invalid params are submitted
-  #       Person.any_instance.stub(:save).and_return(false)
-  #       put :update, {:id => person.to_param, :person => { "first_name" => "invalid value" }}, valid_session
-  #       response.should render_template("edit")
-  #     end
-  #   end
-  # end
-  #
-  # describe "DELETE destroy" do
-  #   it "destroys the requested person" do
-  #     person = Person.create! valid_attributes
-  #     expect {
-  #       delete :destroy, {:id => person.to_param}, valid_session
-  #     }.to change(Person, :count).by(-1)
-  #   end
-  #
-  #   it "redirects to the people list" do
-  #     person = Person.create! valid_attributes
-  #     delete :destroy, {:id => person.to_param}, valid_session
-  #     response.should redirect_to(people_url)
-  #   end
-  # end
+      it "re-renders the 'new' template" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        Person.any_instance.stub(:save).and_return(false)
+        post :create, {:person => { "first_name" => "invalid value" }}, valid_session
+        response.should render_template("new")
+      end
+    end
+  end
 
+  describe "PUT update" do
+    describe "with valid params" do
+      it "updates the requested person" do
+        person = Person.create! valid_attributes
+        # Assuming there are no other people in the database, this
+        # specifies that the Person created on the previous line
+        # receives the :update_attributes message with whatever params are
+        # submitted in the request.
+        Person.any_instance.should_receive(:update).with({ "first_name" => "MyString" })
+        put :update, {:id => person.to_param, :person => { "first_name" => "MyString" }}, valid_session
+      end
+
+      it "assigns the requested person as @person" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => valid_attributes}, valid_session
+        assigns(:person).should eq(person)
+      end
+
+      it "redirects to the person" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => valid_attributes}, valid_session
+        response.should redirect_to(person)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the person as @person" do
+        person = Person.create! valid_attributes
+        # Trigger the behavior that occurs when invalid params are submitted
+        Person.any_instance.stub(:save).and_return(false)
+        put :update, {:id => person.to_param, :person => { "first_name" => "invalid value" }}, valid_session
+        assigns(:person).should eq(person)
+      end
+
+      it "re-renders the 'edit' template" do
+        person = Person.create! valid_attributes
+        # Trigger the behavior that occurs when invalid params are submitted
+        Person.any_instance.stub(:save).and_return(false)
+        put :update, {:id => person.to_param, :person => { "first_name" => "invalid value" }}, valid_session
+        response.should render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "destroys the requested person" do
+      person = Person.create! valid_attributes
+      expect {
+        delete :destroy, {:id => person.to_param}, valid_session
+      }.to change(Person, :count).by(-1)
+    end
+
+    it "redirects to the people list" do
+      person = Person.create! valid_attributes
+      delete :destroy, {:id => person.to_param}, valid_session
+      response.should redirect_to(people_url)
+    end
+  end
 end
